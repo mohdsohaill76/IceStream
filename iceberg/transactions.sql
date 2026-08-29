bef22cefa2074584da872942da08acda8203063a
Venkata Raviteja <venkataraviteja760@gmail.com>
2026-08-29 17:36:53 +0530

    transactions table in SQL

---
+++ iceberg/transactions.sql
@@ -0,0 +1,9 @@
+CREATE TABLE transactions (
+    transaction_id STRING,
+    customer_id STRING,
+    product_id STRING,
+    amount DOUBLE,
+    tax_amount DOUBLE,
+    region STRING,
+    event_time TIMESTAMP(3)
+);
